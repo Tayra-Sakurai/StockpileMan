c1a41eede99ea8ec95f4be77c3d1f725fa0d2bfc
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-05-24 13:59:30 +0900

    Added license headers.

--- stockpileman/src/Components/Views/ViewBase.jsx
+++ stockpileman/src/Components/Views/ViewBase.jsx
@@ -1,3 +1,15 @@
+/**
+ * @fileoverview This is a part of StockpileMan
+ * @copyright Copyright (C) 2026 Tayra Sakurai
+ * @license This is a part of StockpileMan
+ * Copyright (C) 2026 Tayra Sakurai
+ * 
+ * StockpileMan is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * StockpileMan is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with StockpileMan. If not, see <https://www.gnu.org/licenses/>.
+ */
 import { Outlet } from "react-router-dom";
 
 function ViewBase() {
